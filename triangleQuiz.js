103a5bbacf4058c16e4cdf1d6f48c6c8bf9c2d43
ckahub <ahuja13ck@gmail.com>
2021-10-01 10:57:47 +0530

    feast
    completed trianglequiz

---
+++ triangleQuiz.js
@@ -0,0 +1,26 @@
+const quizForm= document.querySelector(".quiz_form");
+const answerBtn= document.querySelector("#submit_answer_btn");
+const output1= document.querySelector("output");
+
+const correctAnswers = ["90","Right Angled Triangle"];
+
+answerBtn.addEventListener("click",calculateScore);
+
+function calculateScore()
+{
+    let score=0;
+    let index=0;
+    const formResult= new FormData(quizForm);
+    console.log(formResult);
+    for(let value of formResult.values())
+    {
+        if(value === correctAnswers[index])
+        {
+            score=score+1;
+        }
+        index=index+1;
+    }
+
+    output1.innerText= "YOUR SCORE IS " +score;
+}
+
