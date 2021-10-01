93d2531584a84ab4f7f0e90a945273601e2fce0d
ckahub <ahuja13ck@gmail.com>
2021-10-01 12:51:12 +0530

    refactor
    area

--- calculateArea.js
+++ calculateArea.js
@@ -1,11 +1,13 @@
-const sideInput= document.querySelector(".side_Input");
+const a1= document.querySelector("#a");
+const b1= document.querySelector("#b");
+const c1= document.querySelector("#c");
 const button=document.querySelector("#btn");
 const output1=document.querySelector("#output");
 
 button.addEventListener("click",CalculateAreaOfTheTriangle);
 
 
-function calculateArea(a,b,c)
+function calculateSArea(a,b,c)
 {
     const s = (a+b+c)/2
     const sArea= (s*((s-a)*(s-b)*(s-c)));
@@ -15,9 +17,9 @@ function calculateArea(a,b,c)
 }
 function CalculateAreaOfTheTriangle()
 {
-   
+   console.log(a1.value,b1.value,c1.value);
   
-    var Area=Math.sqrt(calculateArea(Number(sideInput[0],sideInput[1],sideInput[2])));
-    output1.innerText = "Area of the triangle is "+Area;
+    const Area= Math.sqrt(calculateSArea(a1.value,b1.value,c1.value));
+    output1.innerText = "Area of the triangle is " +Area; 
 }
 
